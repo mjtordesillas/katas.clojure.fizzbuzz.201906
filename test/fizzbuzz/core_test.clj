611a76d0dacfd3398f27ec83d272d325ca786cd9
mjtordesillas <you@example.com>
2019-06-11 08:54:23 +0200

    changed samples to evaluate only the case explained on the test

--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -7,24 +7,24 @@
     (is (= "1" (nth (fizzbuzz) 0)))
     (is (= "26" (nth (fizzbuzz) 25))))
   (testing "A number that is a multiple of 3 is Fizz"
-    (is (= "Fizz" (nth (fizzbuzz) 2)))
+    (is (= "Fizz" (nth (fizzbuzz) 5)))
     (is (= "Fizz" (nth (fizzbuzz) 8))))
   (testing "A number that contains 3 is Fizz"
     (is (= "Fizz" (nth (fizzbuzz) 12)))
     (is (= "Fizz" (nth (fizzbuzz) 22))))
   (testing "A number that is a multiple of 5 or contains a 5 is Buzz"
-    (is (= "Buzz" (nth (fizzbuzz) 4)))
-    (is (= "Buzz" (nth (fizzbuzz) 24))))
+    (is (= "Buzz" (nth (fizzbuzz) 9)))
+    (is (= "Buzz" (nth (fizzbuzz) 39))))
   (testing "A number that contains 5 is Buzz"
     (is (= "Buzz" (nth (fizzbuzz) 51)))
     (is (= "Buzz" (nth (fizzbuzz) 55))))
   (testing "A number that is a multiple of 3 and 5 is FizzBuzz"
     (is (= "FizzBuzz" (nth (fizzbuzz) 29)))
-    (is (= "FizzBuzz" (nth (fizzbuzz) 14))))
+    (is (= "FizzBuzz" (nth (fizzbuzz) 59))))
   (testing "A number that is a multiple of 3 and contains 5 is FizzBuzz"
     (is (= "FizzBuzz" (nth (fizzbuzz) 50))))
   (testing "A number that is a multiple of 5 and contains 3 is FizzBuzz"
-    (is (= "FizzBuzz" (nth (fizzbuzz) 34))))
+    (is (= "FizzBuzz" (nth (fizzbuzz) 29))))
   (testing "A number that contains 5 and 3 is FizzBuzz"
     (is (= "FizzBuzz" (nth (fizzbuzz) 52))))
   (testing "Returns 100 numbers")
