2693c048aae4f09965ca6bd896b6e160ea4fee81
mjtordesillas <you@example.com>
2019-06-11 08:50:36 +0200

    changed tests to check each one of the cases separately

--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -6,14 +6,16 @@
   (testing "A number that is not Fizz or Buzz will be returned as itself"
     (is (= "1" (nth (fizzbuzz) 0)))
     (is (= "26" (nth (fizzbuzz) 25))))
-  (testing "A number that is a multiple of 3 or contains a 3 is Fizz"
+  (testing "A number that is a multiple of 3 is Fizz"
     (is (= "Fizz" (nth (fizzbuzz) 2)))
-    (is (= "Fizz" (nth (fizzbuzz) 8)))
+    (is (= "Fizz" (nth (fizzbuzz) 8))))
+  (testing "A number that contains 3 is Fizz"
     (is (= "Fizz" (nth (fizzbuzz) 12)))
     (is (= "Fizz" (nth (fizzbuzz) 22))))
   (testing "A number that is a multiple of 5 or contains a 5 is Buzz"
     (is (= "Buzz" (nth (fizzbuzz) 4)))
-    (is (= "Buzz" (nth (fizzbuzz) 24)))
+    (is (= "Buzz" (nth (fizzbuzz) 24))))
+  (testing "A number that contains 5 is Buzz"
     (is (= "Buzz" (nth (fizzbuzz) 51)))
     (is (= "Buzz" (nth (fizzbuzz) 55))))
   (testing "A number that is a multiple of 3 and 5 is FizzBuzz"
